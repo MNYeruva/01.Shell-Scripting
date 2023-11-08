d3bdb63747e6a06e07cb2e16f592971b37164bc1
maha <mahanandi.y@royalcyber.com>
2023-11-08 10:55:27 +0530

    updatedByMaha

--- welcome.sh
+++ welcome.sh
@@ -1,3 +1,8 @@
 #!bin/bash
 
 echo "Welcome to MNYeruva"
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: i am good, how are you Ramesh"
