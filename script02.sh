b7284adbe7d4f4f91d190204b205a91d1904e2e9
maha <mahanandi.y@royalcyber.com>
2023-11-08 11:41:12 +0530

    updated

--- script02.sh
+++ script02.sh
@@ -4,7 +4,7 @@ echo "Welcome to MNYeruva"
 P1=Sachin
 P2=Rohit
 
-echo "$R1: Hi $P2, Good Morning"
-echo "$P2: Good Morning $R1"
-echo "$R1: How are you $P2"
-echo "$P2: i am good, how are you $R1"
+echo "$P1: Hi $P2, Good Morning"
+echo "$P2: Good Morning $P1"
+echo "$$P1: How are you $P2"
+echo "$P2: i am good, how are you $P1"
