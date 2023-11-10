25a342ba245413b3c42e91ed1250075df9a0b4b0
maha <mahanandi.y@royalcyber.com>
2023-11-10 09:58:27 +0530

    commit to main

--- 08.git-install.sh
+++ 08.git-install.sh
@@ -13,6 +13,14 @@
 
   yum install git -y
 
+  if [ $? -ne 0 ]
+  then
+    echo "Installation of GIT is error"
+    exit 1
+  else
+    echo "Installation of GIT is success"
+  fi    
+
   # $?=0 it is success
   # $?= not 0 it is failure
   # echo $? 
