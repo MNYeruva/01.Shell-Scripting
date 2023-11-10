f7d1cf3a858edd5ae246d856dfb172dfff225776
maha <mahanandi.y@royalcyber.com>
2023-11-11 04:34:19 +0530

    commite to main

---
+++ practice02.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+DATE=$(date +%F)
+LOGSDIR=/home/centos/sjellscript-logs
+#/home/centos/sjellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y=="\e[33m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo -e \e[31m "ERROR::Please run the script with ROOR user"
+    exit 1
+fi    
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e"Installing $2 ... $R Failure $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G Success $N"   
+    fi     
+}
+#all arguements are in $@
+
+for i in $@
+do
+    yum list installed $id &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed. let's install..."
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else    
+        ehco -e "$Y $i is already installed $N"
