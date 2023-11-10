8f0c0cb22c6236555d97307c8e8018df7a1d8c16
maha <mahanandi.y@royalcyber.com>
2023-11-10 09:46:28 +0530

    commit to main

--- 08.git-install.sh
+++ 08.git-install.sh
@@ -6,8 +6,14 @@
  if [ $USERID -ne 0 ]
  then
     echo "ERROR: Run this script with ROOT user"
+    exit 1
  else   
     echo "This script successfully run by ROOT user"
   fi  
 
   yum install git -y
+
+  # $?=0 it is success
+  # $?= not 0 it is failure
+  # echo $? 
+  # exit 1
