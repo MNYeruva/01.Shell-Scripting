16cccce11baf12873816c39d0fcd5de57650fc0b
maha <mahanandi.y@royalcyber.com>
2023-11-10 10:33:16 +0530

    commit to main

---
+++ 09.functions.sh
@@ -0,0 +1,31 @@
+ #!/bin/bash
+ # install git
+
+ USERID=$(id -u)
+ # This Function should validate the previous command and inform user it is success OR failure
+ VALIDATE(){
+    #$1 ---> IT WILL RECEIVE THE ARGUMENT1
+      if [ $? -ne 0 ]
+    then
+        echo "Installation... FAILURE"
+        exit 1
+    else
+        echo "Installation... SUCCESS"
+    fi  
+ }
+
+ if [ $USERID -ne 0 ]
+ then
+    echo "ERROR: Run this script with ROOT user"
+    exit 1
+ else   
+    echo "This script successfully run by ROOT user"
+  fi  
+
+  yum install git -y
+
+    VALIDATE $?
+
+  yum install mysql -y
+
+    VALIDATE $?
