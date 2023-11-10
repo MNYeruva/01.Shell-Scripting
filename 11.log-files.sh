4477f7c0145ac0dffb8328f97571eb10fe234385
maha <mahanandi.y@royalcyber.com>
2023-11-11 00:11:22 +0530

    commite to main

--- 11.log-files.sh
+++ 11.log-files.sh
@@ -3,15 +3,21 @@
   DATE=$(date +%F)   #$(date +%F:%H:%M:%S)
   SCRIPT_NAME=$0
   LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+ #Colors vars
+ R="\e[31m" 
+ G="\e[31m"
+ N="\e[0m"
+
    # This Function should validate the previous command and inform user it is success OR failure
    VALIDATE(){
     #$1 ---> IT WILL RECEIVE THE ARGUMENT1
       if [ $1 -ne 0 ]
     then
-        echo "$2... FAILURE"
+        echo -e "$2... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2... \e[32m SUCCESS"
+        echo -e "$2... $G SUCCESS $N"    #\e[31m --> With color enabling...
     fi  
  }
 
---
+++ 11.loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 1
+echo 2
+echo 3
+echo 4
+echo 5
+
+# Create Lopps
+
+for i {1..100}
+do
+    echo $1
+done    
