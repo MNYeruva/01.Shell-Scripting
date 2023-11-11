10339a5e3f4dd1394bafc77727f5dbf51a2b74c6
maha <mahanandi.y@royalcyber.com>
2023-11-11 10:40:02 +0530

    commite to main

--- 13.delete-old-log.sh
+++ 13.delete-old-log.sh
@@ -2,7 +2,7 @@
 
 APP_LOGS_DIR=/home/centos/apps-logs
 
-DATE=$(date +%F)   #$(date +%F:%H:%M:%S)
+DATE=$(date +%F:%H:%M:%S)
 LOGSDIR=/home/centos/shellscript-logs
 # /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
@@ -10,12 +10,13 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +30)
 
-echo "Script started to executing $DATE"
+echo "Script started to executing $DATE" &>>$LOGFILE
 #echo "$FILES_TO_DELETE"
 
 #While LOOP
 
 while read line 
 do
-    echo "Deleting $line" &>> $LOGFILE    #we write our statements here..
+    echo "Deleting $line" &>>$LOGFILE    #we write our statements here..
+    rm -rf $line
 done <<< $FILES_TO_DELETE   
