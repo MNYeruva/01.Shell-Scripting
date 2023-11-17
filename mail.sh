aa25a4f77d2100469d334ad86885a00c2fb4a8f9
maha <mahanandi.y@royalcyber.com>
2023-11-17 23:16:42 +0530

    Template.html

--- mail.sh
+++ mail.sh
@@ -3,13 +3,14 @@
 
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
+echo "escaped content: $BODY"
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
 #echo "all agruements" $@"
 
-FINAL_BODY=$(sed -e 's/TEAM_NAME/DevOps Team/g' -e 's/ALERT_TYPE/High Disk Usage' -e "s/MESSAGE/$BODY" -e template.html)
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" -e template.html)
 
 #echo "$MESSAGE  Date $(date)" | mail -s "High Disk Usage Alert" info@mnyeruva.com
 
